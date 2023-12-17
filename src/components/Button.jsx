1868f52a414b9095eb7eef44118154bb9722214c
Badr <moslihbadr2@gmail.com>
2023-12-17 02:18:29 +0000

    add Button component

---
+++ src/components/Button.jsx
@@ -0,0 +1,24 @@
+import { styled } from "styled-components"
+
+const Button = ({ children, className, handleClick }) => {
+  return (
+    <StyledButton className={`${className} btn`} onClick={() => handleClick()}>
+      {children}
+    </StyledButton>
+  )
+}
+
+const StyledButton = styled.button`
+  transition: ${(props) => props.theme.transition};
+  background-color: ${(props) => props.theme.buttonText};
+  color: ${(props) => props.theme.buttonBg};
+  border: ${(props) => props.theme.borderColor} 2px solid;
+  padding: 0.5rem 1rem;
+
+  &:hover {
+    color: ${(props) => props.theme.buttonText};
+    background-color: ${(props) => props.theme.buttonBg};
+  }
+`;
+
+export default Button
