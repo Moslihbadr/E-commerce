e80c9489a1de4d82f50ba5faa8456a7834f0d087
Badr <moslihbadr2@gmail.com>
2023-12-17 02:23:44 +0000

    add Spinner component

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,11 @@
+const Spinner = () => {
+  return (
+    <div className="d-flex justify-content-center">
+      <div className="spinner-border m-5" role="status">
+        <span className="visually-hidden">Loading...</span>
+      </div>
+    </div>
+  )
+}
+
+export default Spinner;
